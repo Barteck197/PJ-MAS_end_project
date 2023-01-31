8dd983e0bdb7d326e531c82cdfb48a84de2ae008
Grzegorz <grzegorz.bardski@cumulusit.pl>
2023-01-31 21:54:03 +0100

    dodanie klasy purchase order

--- src/main/java/model/PurchaseOrder.java
+++ src/main/java/model/PurchaseOrder.java
@@ -1,4 +1,22 @@
 package model;
 
+import java.time.LocalDate;
+
 public class PurchaseOrder {
+    private static int nextPurchaseOrderID = 0;
+
+    private int purchaseOrderNumber;
+    private LocalDate orderPlaceDate;
+    private int quantity;
+
+    /**
+     * Konstruktor klasy: Zamówienie
+     * @param orderPlaceDate Data złożenia zamówienia
+     * @param quantity Ilość
+     */
+    public PurchaseOrder(LocalDate orderPlaceDate, int quantity) {
+        this.orderPlaceDate = orderPlaceDate;
+        this.quantity = quantity;
+        this.purchaseOrderNumber = nextPurchaseOrderID++;
+    }
 }

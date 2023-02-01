334dba2bfd1226d6b1d5e307ad6d23387d7c9963
Grzegorz <grzegorz.bardski@cumulusit.pl>
2023-02-01 22:56:43 +0100

    dodanie widoku dodawania serwera

--- src/main/java/gb/DashboardController.java
+++ src/main/java/gb/DashboardController.java
@@ -1,4 +1,32 @@
 package gb;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
 public class DashboardController {
+
+    @FXML
+    private Button addServerButton;
+
+    @FXML
+    void addServerButtonClicked(ActionEvent event){
+        try {
+            // Load the FXML file
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("add-server-view.fxml")));
+
+            // Create a new stage
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root, 680, 480));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }

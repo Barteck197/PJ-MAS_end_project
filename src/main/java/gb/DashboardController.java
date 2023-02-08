4037d873e7b49e9033e5ad20de38a68759019e9e
Grzegorz <grzegorz.bardski@cumulusit.pl>
2023-02-09 00:00:41 +0100

    Dodanie listenera na widoku serwerów

--- src/main/java/gb/DashboardController.java
+++ src/main/java/gb/DashboardController.java
@@ -47,11 +47,13 @@ public class DashboardController {
         ObservableList<Server> serverObservableList = FXCollections.observableArrayList(servers);
         serverTableView.setItems(serverObservableList);
 
-        serverTableView.getSelectionModel().selectedItemProperty().addListener((observableValue, server, t1) -> {
-            try {
-                showServer(t1);
-            } catch (Exception e) {
-                e.printStackTrace();
+        serverTableView.setOnMouseClicked(mouseEvent -> {
+            if (mouseEvent.getClickCount() == 2) {
+                try {
+                    showServer(serverTableView.getSelectionModel().getSelectedItem());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
     }
@@ -60,27 +62,12 @@ public class DashboardController {
     protected void showServer(Server server) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("server-view.fxml"));
         // Podczas ładowania nowego okna w konstruktorze przekazujemy obiekt książki do wyświetlenia
-        loader.setControllerFactory(controller -> new ServerController(server));
+        loader.setControllerFactory(controller -> new ServerController(server, server.getServerRack()));
         Pane myPane = loader.load();
 
         Stage newStage = new Stage();
         newStage.setScene(new Scene(myPane));
         newStage.show();
-    }
-
-    @FXML
-    void addServerButtonClicked(ActionEvent event) {
-        try {
-            // Load the FXML file
-            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("server-view.fxml")));
-
-            // Create a new stage
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root, 680, 480));
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     @FXML
